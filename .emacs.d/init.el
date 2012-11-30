ada71becd87a68bec24bc37f49af56b35a9cac56
kskotetsu <kskotetsu@gmail.com>
2012-11-30 16:16:17 +0900

    [emacs]popwin, shell(cygwin bash) setting added

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -132,6 +132,11 @@
 (color-theme-molokai)
 
 ;; ------------------------------------------------------------------------
+;; popwin
+(require 'popwin)
+(setq display-buffer-function 'popwin:display-buffer)
+
+;; ------------------------------------------------------------------------
 ;; helm-mode
 (global-set-key [?\C-;] 'helm-mini)
 (helm-mode 1)
@@ -147,6 +152,51 @@
 (global-auto-complete-mode t)
 
 (global-set-key [?\M-/] 'auto-complete)
+
+;; ------------------------------------------------------------------------
+;; シェル
+(require 'cygwin-mount)
+(cygwin-mount-activate)
+;; shellの文字化けを回避
+;; (add-hook 'shell-mode-hook
+;;           (lambda ()
+;;             (set-buffer-process-coding-system 'utf-8-unix 'utf-8-unix)
+;;             ))
+
+(setq shell-mode-hook
+      (function
+       (lambda ()
+
+	 ;; シェルモードの入出力文字コード
+	 (set-buffer-process-coding-system 'sjis-dos 'sjis-unix)
+	 (set-buffer-file-coding-system    'sjis-unix)
+	 )))
+
+(setq cygwin-mount-cygwin-bin-directory
+      (concat (getenv "CYGWIN_DIR") "\\bin"))
+(require 'setup-cygwin)
+
+;; ------------------------------------------------------------------------
+;; @ shell
+(require 'shell)
+(setq explicit-shell-file-name "bash.exe")
+(setq shell-command-switch "-c")
+(setq shell-file-name "bash.exe")
+
+;; (M-! and M-| and compile.el)
+(setq shell-file-name "bash.exe")
+(modify-coding-system-alist 'process ".*sh\\.exe" 'cp932)
+
+;; shellモードの時の^M抑制
+(add-hook 'comint-output-filter-functions 'shell-strip-ctrl-m nil t)
+
+;; shell-modeでの補完 (for drive letter)
+(setq shell-file-name-chars "~/A-Za-z0-9_^$!#%&{}@'`.,;()-")
+
+;; エスケープシーケンス処理の設定
+(autoload 'ansi-color-for-comint-mode-on "ansi-color"
+  "Set `ansi-color-for-comint-mode' to t." t)
+
 
 ;; ------------------------------------------------------------------------
 ;; org-mode
